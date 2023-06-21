e108836e2f462b7da716bccf8321960d4207ee44
Codablack <goodluckedih@yahoo.com>
2023-06-21 18:47:03 +0100

    reduced light intensity more

--- src/views/Light.tsx
+++ src/views/Light.tsx
@@ -1,27 +1,27 @@
 export default function Lighting(){
     return <>
      <spotLight  intensity={1} angle={0.99} penumbra={1} decay={2} position={[-0.565, 5.068, 8.636]} rotation={[0.581, 0.002, -1.379]} scale={-1} />
-      <spotLight intensity={3}  angle={0.99} penumbra={1} decay={2} position={[-11.722, 6.086, 0.738]} rotation={[1.782, -1.019, 0.197]} scale={-1} />
-      <spotLight intensity={5}  angle={0.99} penumbra={1} decay={2} position={[11.404, 6.086, 0.738]} rotation={[-1.359, -1.019, 0.197]} />
-      <spotLight intensity={5} angle={0.577} penumbra={1} decay={2} position={[3.036, 3.77, 1.459]} rotation={[-0.761, 0.787, -0.352]} />
-      <spotLight intensity={5} angle={0.778} penumbra={1} decay={2} color="#e8fffe" position={[-4.397, 4.022, 4.811]} rotation={[-0.604, -0.74, 1.055]} />
-      <spotLight intensity={6} angle={0.884} penumbra={1} decay={2} color="#e8fffe" position={[-2.34, 4.973, -4.552]} rotation={[-2.091, -0.381, 0.057]} />
-      <pointLight intensity={6}  decay={2} color="#e4ff6b" position={[14.929, 4.894, -19.195]} rotation={[-Math.PI / 2, 0, 0]} />
-      <pointLight intensity={5}  decay={2} color="#89ffcd" position={[14.929, 4.894, 0.724]} rotation={[-Math.PI / 2, 0, 0]} />
-      <pointLight intensity={4}  decay={2} color="#ffd368" position={[14.929, 4.894, 19.789]} rotation={[-Math.PI / 2, 0, 0]} />
-      <pointLight intensity={4} decay={2} color="#a0c9ff" position={[4.614, 7.581, 18.063]} rotation={[-Math.PI / 2, 0, 0]} />
-      <pointLight intensity={10} decay={2} color="#a0c9ff" position={[-5.996, 7.581, 19.978]} rotation={[-Math.PI / 2, 0, 0]} />
-      <pointLight intensity={11}  decay={2} color="#e4ff6b" position={[-16.779, 4.894, -19.195]} rotation={[-Math.PI / 2, 0, 0]} />
-      <pointLight intensity={15}  decay={2} position={[2.205, 2.524, -3.281]} rotation={[-Math.PI / 2, 0, 0]} />
-      <pointLight intensity={10} decay={2} color="#ffd368" position={[-16.779, 4.894, 19.789]} rotation={[-Math.PI / 2, 0, 0]} />
-      <pointLight intensity={12} decay={2} position={[-5.989, 5.904, -1.005]} rotation={[-1.839, 0.602, 1.932]} />
-      <pointLight intensity={9}  decay={2} color="#a0c9ff" position={[-5.996, 7.581, 19.978]} rotation={[-Math.PI / 2, 0, 0]} />
-      <pointLight intensity={10} decay={2} color="#ffd368" position={[-16.779, 4.894, 19.789]} rotation={[-Math.PI / 2, 0, 0]} />
-      <pointLight intensity={8}  decay={2} color="#a0c9ff" position={[4.614, 7.581, -19.24]} rotation={[-Math.PI / 2, 0, 0]} />
-      <pointLight intensity={9}  decay={2} color="#a0c9ff" position={[-5.996, 7.581, -19.24]} rotation={[-Math.PI / 2, 0, 0]} />
-      <pointLight intensity={10}  decay={2} color="#89ffcd" position={[-16.415, 4.894, 0.724]} rotation={[-Math.PI / 2, 0, 0]} />
-      <pointLight intensity={11}  decay={2} color="#fcf2ff" position={[4.964, 7.581, 0.239]} rotation={[-Math.PI / 2, 0, 0]} />
-      <pointLight intensity={12} decay={2} color="#fcf2ff" position={[-6.024, 7.581, 0.239]} rotation={[-Math.PI / 2, 0, 0]} />
-      <pointLight intensity={13}  decay={2} position={[4.076, 5.904, -1.005]} rotation={[-1.839, 0.602, 1.932]} />
+      <spotLight intensity={2}  angle={0.99} penumbra={1} decay={2} position={[-11.722, 6.086, 0.738]} rotation={[1.782, -1.019, 0.197]} scale={-1} />
+      <spotLight intensity={2}  angle={0.99} penumbra={1} decay={2} position={[11.404, 6.086, 0.738]} rotation={[-1.359, -1.019, 0.197]} />
+      <spotLight intensity={2} angle={0.577} penumbra={1} decay={2} position={[3.036, 3.77, 1.459]} rotation={[-0.761, 0.787, -0.352]} />
+      <spotLight intensity={2} angle={0.778} penumbra={1} decay={2} color="#e8fffe" position={[-4.397, 4.022, 4.811]} rotation={[-0.604, -0.74, 1.055]} />
+      <spotLight intensity={2} angle={0.884} penumbra={1} decay={2} color="#e8fffe" position={[-2.34, 4.973, -4.552]} rotation={[-2.091, -0.381, 0.057]} />
+      <pointLight intensity={2}  decay={2} color="#e4ff6b" position={[14.929, 4.894, -19.195]} rotation={[-Math.PI / 2, 0, 0]} />
+      <pointLight intensity={2}  decay={2} color="#89ffcd" position={[14.929, 4.894, 0.724]} rotation={[-Math.PI / 2, 0, 0]} />
+      <pointLight intensity={2}  decay={2} color="#ffd368" position={[14.929, 4.894, 19.789]} rotation={[-Math.PI / 2, 0, 0]} />
+      <pointLight intensity={2} decay={2} color="#a0c9ff" position={[4.614, 7.581, 18.063]} rotation={[-Math.PI / 2, 0, 0]} />
+      <pointLight intensity={1} decay={2} color="#a0c9ff" position={[-5.996, 7.581, 19.978]} rotation={[-Math.PI / 2, 0, 0]} />
+      <pointLight intensity={1}  decay={2} color="#e4ff6b" position={[-16.779, 4.894, -19.195]} rotation={[-Math.PI / 2, 0, 0]} />
+      <pointLight intensity={1}  decay={2} position={[2.205, 2.524, -3.281]} rotation={[-Math.PI / 2, 0, 0]} />
+      <pointLight intensity={1} decay={2} color="#ffd368" position={[-16.779, 4.894, 19.789]} rotation={[-Math.PI / 2, 0, 0]} />
+      <pointLight intensity={1} decay={2} position={[-5.989, 5.904, -1.005]} rotation={[-1.839, 0.602, 1.932]} />
+      <pointLight intensity={1}  decay={2} color="#a0c9ff" position={[-5.996, 7.581, 19.978]} rotation={[-Math.PI / 2, 0, 0]} />
+      <pointLight intensity={1} decay={2} color="#ffd368" position={[-16.779, 4.894, 19.789]} rotation={[-Math.PI / 2, 0, 0]} />
+      <pointLight intensity={1}  decay={2} color="#a0c9ff" position={[4.614, 7.581, -19.24]} rotation={[-Math.PI / 2, 0, 0]} />
+      <pointLight intensity={2}  decay={2} color="#a0c9ff" position={[-5.996, 7.581, -19.24]} rotation={[-Math.PI / 2, 0, 0]} />
+      <pointLight intensity={1}  decay={2} color="#89ffcd" position={[-16.415, 4.894, 0.724]} rotation={[-Math.PI / 2, 0, 0]} />
+      <pointLight intensity={1}  decay={2} color="#fcf2ff" position={[4.964, 7.581, 0.239]} rotation={[-Math.PI / 2, 0, 0]} />
+      <pointLight intensity={1} decay={2} color="#fcf2ff" position={[-6.024, 7.581, 0.239]} rotation={[-Math.PI / 2, 0, 0]} />
+      <pointLight intensity={1}  decay={2} position={[4.076, 5.904, -1.005]} rotation={[-1.839, 0.602, 1.932]} />
     </>
 }
